9cc3e6cca7e149ed69991de237bbb7081daf70ed
Leonardo <leofelipesc@gmail.com>
2026-05-31 14:44:10 -0300

    Organizando scripts e rascunhos em subpastas

--- Códigos/analise_pesquisa.py
+++ Códigos/analise_pesquisa.py
@@ -3,4 +3,10 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('./Dados/pesquisa_marketing.csv')
 
-print(df)
+#print(df)
+print(df["plataforma_favorita"].value_counts()).plot(kind="bar")
+
+
+
+
+
